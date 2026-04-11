444ec57208f02dc9277c40b6c70a8fb54898e2c3
John Hopwood <johnhopwood@Mac.fritz.box>
2026-04-11 14:11:01 +0100

    Reduce toggle switch translate distance to 8px
    - Circle was too far to the left with 14px translate
    - Reduced to 8px for better positioning

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1286,7 +1286,7 @@ function ToggleSwitch({
     >
       <span
         className={`inline-block h-5 w-5 transform rounded-full bg-white transition-transform ${
-          checked ? "translate-x-[14px]" : "-translate-x-[14px]"
+          checked ? "translate-x-[8px]" : "-translate-x-[8px]"
         }`}
       />
     </button>
